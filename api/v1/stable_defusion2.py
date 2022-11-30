80a307007ba575b0f0cbdc00df733b0deb391dd1
Vishal Bende <vishal.bende06@gmail.com>
2022-11-30 23:45:23 +0530

    feat: model integrations

--- api/v1/stable_defusion2.py
+++ api/v1/stable_defusion2.py
@@ -8,6 +8,20 @@ from settings import HUGGINGFACE_STABLE_DIFFUSION_API_URL, HUGGINGFACE_API_TOKEN
 from fastapi.responses import FileResponse
 
 
+from diffusers import DiffusionPipeline
+import torch
+#pipeline = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2")
+
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+
+model_id = "stabilityai/stable-diffusion-2"
+
+# Use the Euler scheduler here instead
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, revision="fp16", torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+
 class Item(BaseModel):
     input_string: str
 
@@ -25,5 +39,8 @@ def get_inference(args: Item):
     #     "inputs": args.input_string
 
     # })
+    prompt = args.input_string
+    image = pipe(prompt, height=768, width=768).images[0]
+    image.save('static/main_image.png')
 
-    return FileResponse('static/cat.jpeg')
+    return FileResponse('static/main_image.png')
